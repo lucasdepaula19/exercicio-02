1e917da8e1d51d8ae3a3744d5f4ce34ca16e158a
Lucas de Paula <lucas.barbosa@microcity.com.br>
2019-08-26 21:19:51 -0300

    Inclusão método search

--- src/components/ComponentListUsers.js
+++ src/components/ComponentListUsers.js
@@ -1,43 +1,58 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
+let usuarios = [
+    { name: 'joao', email: 'joao@email.com', age: 32 },
+    { name: 'pedro', email: 'edro@email.com', age: 33 },
+    { name: 'lucas', email: 'lucas@email.com', age: 34 },
+    { name: 'tata', email: 'tata@email.com', age: 35 },
+    { name: 'antonio', email: 'antonio@email.com', age: 33 },
+    { name: 'jose', email: 'jose@email.com', age: 64 },
+    { name: 'vera', email: 'vera@email.com', age: 63 },
+    { name: 'luciano', email: 'luciano@email.com', age: 64 },
+    { name: 'luma duarte', email: 'luma@email.com', age: 63 }
+];
+
 export class ListUsers extends React.Component {
 
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            usuarios: [
-                { name: 'joao', email: 'joao@email.com', age: 32 },
-                { name: 'pedro', email: 'edro@email.com', age: 33 },
-                { name: 'lucas', email: 'lucas@email.com', age: 34 },
-                { name: 'tata', email: 'tata@email.com', age: 35 },
-                { name: 'antonio', email: 'antonio@email.com', age: 33 },
-                { name: 'jose', email: 'jose@email.com', age: 64 },
-                { name: 'vera', email: 'vera@email.com', age: 63 }
-            ]
+            users: usuarios,
+            result: usuarios,
         }
+        this.search = this.search.bind(this);
+    }
+
+    del(e, param) {
+        let users = this.state.users;
+
+        for (var i = 0; i < users.length; i++) {
+            if (users[i].name === param) {
+                users.splice(i, 1);
+            }
+        }
+        this.setState({ users });
+        e.preventDefault();
+    }
+
+    search(event) {
+        let value = event.target.value;
+        let users = this.state.users, result = [];
+        result = users.filter((user) => {
+            return user.name.toLowerCase().search(value) !== -1;
+        });
+        this.setState({ result });
     }
 
     viewForm() {
-        function del(e, param) {
-            e.preventDefault();
-            console.log(param);
-            for(var i=0 ; i < this.state.usuarios.length; i++) {
-            
-                //console.log(this.usuarios[i].value);
-                // if(this.usuarios[i].value==param)
-                //     this.usuarios.splice(i);
-                // console.log(this.usuarios[i]);
-            }
-        }
-
-        return this.state.usuarios.map((usuarios, index) => {
+        return this.state.result.map((usuarios) => {
             const { name, email, age } = usuarios
             return (
                 <tr>
                     <td>
                         <Link to={'/users/' + name}>{name}</Link>
-                        <button onClick={(e) => del(e, name)}>Delete</button>
+                        <button onClick={(e) => this.del(e, name)}>Delete</button>
                     </td>
                     <td>{email}</td>
                     <td>{age}</td>
@@ -47,29 +62,25 @@ export class ListUsers extends React.Component {
     }
 
     render() {
-        const nome = this.props.match.params.name;
-        if (nome) {
-            return (<div><busca /><h1 id='title'>{nome}</h1></div>)
-        } else {
-            return (
-                <>
-                    <div>
-                        <h2>Users table</h2>
-                        <table id='users' className="formatTable">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Age</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.viewForm()}
-                            </tbody>
-                        </table>
-                    </div>
-                </>
-            )
-        }
+        return (
+            <>
+                <input type="text" placeholder="Search" onChange={this.search} />
+                <div>
+                    <h2>Users table</h2>
+                    <table id='users' className="formatTable">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Age</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.viewForm()}
+                        </tbody>
+                    </table>
+                </div>
+            </>
+        )
     }
 }
